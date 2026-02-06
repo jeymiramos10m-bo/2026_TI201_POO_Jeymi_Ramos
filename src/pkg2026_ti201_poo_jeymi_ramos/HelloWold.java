a77481b3de777b778cf605e84b147bcb72a21eaf
Yeimy <Yeimy@DESKTOP-543M4BC.mshome.net>
2026-02-06 11:04:48 -0600

    Inicio de Proyecto ITESS-POO

---
+++ src/pkg2026_ti201_poo_jeymi_ramos/HelloWold.java
@@ -0,0 +1,16 @@
+/**
+ * 2026 ITESS-TICS
+ * Periodo Enero Junio
+ * Asignatura: Programacion Orientado a objetos
+ * Docente: Francisco Javier Montecillo Puente
+ * Descripcion: Tema 1. Entorno de desarrollo 
+ *              Prueva del entorno de desarrolloy github
+ * Intento Programador: Jeymi Jimena Ramos Martínez
+ * Fecha: 06 de febrero del 2026
+ */
+package pkg2026_ti201_poo_jeymi_ramos;
+public class HelloWold {
+    public void sayHello(String name) {
+        System.out.print("Hello" +name);
+    }
+}
---
+++ src/pkg2026_ti201_poo_jeymi_ramos/HellowoldApp.java
@@ -0,0 +1,20 @@
+/*
+ * 2026 ITESS-TICS
+ * Periodo Enero Junio
+ * Asignatura: Programacion Orientado a objetos
+ * Docente: Francisco Javier Montecillo Puente
+ * Descripcion: Tema 1. Entorno de desarrollo 
+ *              Prueva del entorno de desarrolloy github
+ * Intento Programador: Jeymi Jimena Ramos Martínez
+ * Fecha: 06 de febrero del 2026
+ */
+package pkg2026_ti201_poo_jeymi_ramos;
+
+public class HellowoldApp {
+    public static void main (String[]args){
+        HelloWold hello = new HelloWold();
+        hello.sayHello("Jeymi Jimena Ramos Martínez");
+        hello.sayHello("ITESS");
+    }
+    
+}
